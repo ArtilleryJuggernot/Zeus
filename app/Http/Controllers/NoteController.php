76f2cfb836bd4c461862ed9a5e17fd42fd528196
ArtilleryJuggernot <hugojuggernot@gmail.com>
2024-05-11 11:40:14 +0200

    adding return on saveNote

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -221,7 +221,7 @@ class NoteController extends Controller
     }
 
     // Avec AJAX
-    public function saveNote(Request $request)
+    public function saveNote(Request $request): \Illuminate\Http\JsonResponse
     {
         // Récupérer le contenu du textarea
 
@@ -234,8 +234,7 @@ class NoteController extends Controller
         ]);
 
 
-        $output = new ConsoleOutput();
-        $output->writeln("After validation OK");
+
 
         $content = $validateData["content"];
         $user_id = $validateData["user_id"];
@@ -259,18 +258,15 @@ class NoteController extends Controller
         $perm_rec = $perm == "RW" || $perm == "F";
 
 
-        $output->writeln($note_id);
 
         if($user_id == Auth::user()->id || $autorisation || $perm_rec){
             $note = Note::find($note_id);
-            $output->writeln("Autorisation OK : ". $note->name);
 
             if($note->owner_id == Auth::user()->id || $autorisation || $perm_rec){
 
                 // Chiffrement des données
 
 
-                $output->writeln("Starting chiffrement");
 
                 $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); // Générer un IV aléatoire
                 $encryptedData = openssl_encrypt($content, "aes-256-cbc", $note->note_key, 0, $iv);
@@ -281,14 +277,13 @@ class NoteController extends Controller
 
                 $check = Storage::put($note->path,$finalDataEncryptedAES);
                 LogsController::saveNote($user_id,$note_id,$note->name,"SUCCESS");
-                $output->writeln("Check is : " . $check);
                 return response()->json(['success' => true]);
             }
-
             LogsController::saveNote($user_id,$note_id,$note->name,"FAILURE");
-
+            return response()->json(['failure' => false]);
 
         }
+        return response()->json(['failure' => false]);
     }
 
 
