b443578e40d675481349a6336f7c39d511e14855
Artillery <jug@jug.fr>
2025-05-04 23:46:46 +0200

    no google login if disabled

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -123,6 +123,7 @@
                         <div class="text-center content-center">
 
 
+                        @if($allow_new_users)
                         <a href="{{ route('google.redirect') }}">
                         <button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
                             <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
@@ -136,6 +137,7 @@
                             <label for="terms" class="txt2">En se connectant via Google, vous acceptez les <a href="/cgu" target="_blank">Conditions Générales d'Utilisation (CGU)</a></label>
 
                         </div>
+                        @endif
 
 
 
